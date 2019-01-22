0019de3726510fc4ebfa028a8957e0c59fbe250d
Erystasius <Felix.E.Huang@gmail.com>
2019-01-22 10:05:04 +0800

    update config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,7 +16,6 @@
 
 (global-evil-leader-mode 1)
 ;; modes
-(which-key-mode 1)
 (evil-mode 1)
 (global-company-mode 1)
 (global-hungry-delete-mode 1)
@@ -39,21 +38,36 @@
 (recentf-mode 1)
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key 
-  "p" 'projectile-command-map
+  ;; Elisp
+  "e e" 'eval-last-sexp
+  "e b" 'eval-buffer
+  ;; Buffer
+  "b l" 'list-buffers
+  "b s" 'switch-to-buffer
+  "b v" 'view-buffer-other-window
+  "b o" 'switch-to-buffer-other-window
+  "b a" 'save-some-buffers
+  "b k" 'kill-this-buffer
+  ;; Window
   "w d" 'delete-window
   "w m" 'delete-other-windows
   "w h" 'split-window-below
   "w v" 'split-window-right
-  "e e" 'eval-last-sexp
+  ;; Window Numbering
+  "1" 'select-window-1
+  "2" 'select-window-2
+  "3" 'select-window-3
+  "4" 'select-window-4
+  ;; File
   "f f" 'find-file
   "f r" 'counsel-recentf
-  "b k" 'kill-buffer
-  "b l" 'list-buffers
-  "b s" 'switch-to-buffer
+  ;; Help
   "h m" 'describe-mode
   "h f" 'describe-function
   "h k" 'describe-key
   "h v" 'describe-variable
+  ;; Misc
+  "p" 'projectile-command-map
   "<SPC>" 'counsel-M-x)
 
 (global-set-key (kbd "<f6>") 'ivy-resume)
@@ -91,7 +105,7 @@
     ("f1e3641bd6cdd4bf571fc27820a2dfd2dd03c8cf0e251e04d5509632dfe6f004" "760ce657e710a77bcf6df51d97e51aae2ee7db1fba21bbad07aab0fa0f42f834" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" "0c3b1358ea01895e56d1c0193f72559449462e5952bded28c81a8e09b53f103f" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "2a998a3b66a0a6068bcb8b53cd3b519d230dd1527b07232e54c8b9d84061d48d" default)))
  '(package-selected-packages
    (quote
-    (window-numbering projectile php-mode popwin counsel ivy swiper hungry-delete evil-escape evil-leader base16-theme company which-key evil pallet))))
+    (window-numbering projectile php-mode popwin counsel ivy swiper hungry-delete evil-escape evil-leader base16-theme company evil pallet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
