f85666167bf19939908f58f8b1a9642c9ec98cb3
Erystasius <Felix.E.Huang@gmail.com>
2019-10-27 06:05:02 +0800

    No-message commit [Generated by ac command]

--- starters/extra.sh
+++ starters/extra.sh
@@ -1,6 +1,6 @@
 # Network
-yay -S --needed --noconfirm shadowsocks
 yay -S --needed --noconfirm v2ray
+yay -S --needed --noconfirm shadowsocks
 
 # Fcitx
 yay -S --needed --noconfirm fcitx-im
@@ -9,29 +9,30 @@ yay -S --needed --noconfirm fcitx-configtool
 # File Sync
 yay -S --needed --noconfirm dropbox
 yay -S --needed --noconfirm nutstore
+yay -S --needed --noconfirm baidunetdisk
 
 # Emacs
-yay -S --needed --noconfirm emacs
 yay -S --needed --noconfirm cask
+yay -S --needed --noconfirm emacs
 
 # Aria2
 yay -S --needed --noconfirm aria2
 yay -S --needed --noconfirm aria2-systemd
 
 # Major Applications
-yay -S --needed --noconfirm chrome
-yay -S --needed --noconfirm oh-my-zsh-git
-yay -S --needed --noconfirm ulauncher
 yay -S --needed --noconfirm anki
-yay -S --needed --noconfirm goldendict
-yay -S --needed --noconfirm mcomix
-yay -S --needed --noconfirm wps-office
-yay -S --needed --noconfirm font-manager
-yay -S --needed --noconfirm baidunetdisk
-yay -S --needed --noconfirm deepin-wine-wechat
-yay -S --needed --noconfirm dingtalk
-yay -S --needed --noconfirm teamviewer
 yay -S --needed --noconfirm xmind
+yay -S --needed --noconfirm chrome
+yay -S --needed --noconfirm mcomix
+yay -S --needed --noconfirm dingtalk
+yay -S --needed --noconfirm ulauncher
+yay -S --needed --noconfirm celluloid
+yay -S --needed --noconfirm goldendict
+yay -S --needed --noconfirm wps-office
+yay -S --needed --noconfirm teamviewer
+yay -S --needed --noconfirm font-manager
+yay -S --needed --noconfirm deepin-wine-wechat
+yay -S --needed --noconfirm oh-my-zsh-git
 
 # Show off
 yay -S --needed --noconfirm gtop
@@ -42,14 +43,14 @@ yay -S --needed --noconfirm neofetch
 yay -S --needed --noconfirm tty-clock
 
 # Programming
-yay -S --needed --noconfirm intellij-idea-ultimate-edition
-yay -S --needed --noconfirm nosqlbooster
-yay -S --needed --noconfirm postman
 yay -S --needed --noconfirm meld
+yay -S --needed --noconfirm postman
+yay -S --needed --noconfirm nosqlbooster
+yay -S --needed --noconfirm intellij-idea-ultimate-edition
 
 # Tools
-yay -S --needed --noconfirm deepin-system-monitor
-yay -S --needed --noconfirm deepin-screenshot
-yay -S --needed --noconfirm deepin-picker
 yay -S --needed --noconfirm imagewriter
+yay -S --needed --noconfirm deepin-picker
+yay -S --needed --noconfirm deepin-screenshot
+yay -S --needed --noconfirm deepin-system-monitor
 
