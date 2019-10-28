a1f8cf14ea72bd090151db016e390ba55a57a54a
Erystasius <Felix.E.Huang@gmail.com>
2019-10-29 00:53:36 +0800

    No-message commit [Generated by ac command]

--- starters/extra.sh
+++ starters/extra.sh
@@ -1,4 +1,13 @@
+# Hardware
+yay -S --needed --noconfirm gestures
+yay -S --needed --noconfirm blueberry
+yay -S --needed --noconfirm pulseaudio
+yay -S --needed --noconfirm pavucontrol
+yay -S --needed --noconfirm network-manager-applet
+
 # Enhancement
+yay -S --needed --noconfirm google-chrome
+yay -S --needed --noconfirm ulauncher
 yay -S --needed --noconfirm autotiling
 yay -S --needed --noconfirm oh-my-zsh-git
 
@@ -10,11 +19,6 @@ yay -S --needed --noconfirm shadowsocks
 yay -S --needed --noconfirm fcitx-im
 yay -S --needed --noconfirm fcitx-configtool
 
-# File Sync
-yay -S --needed --noconfirm dropbox
-yay -S --needed --noconfirm nutstore
-yay -S --needed --noconfirm baidunetdisk
-
 # Emacs
 yay -S --needed --noconfirm cask
 yay -S --needed --noconfirm emacs
@@ -23,37 +27,41 @@ yay -S --needed --noconfirm emacs
 yay -S --needed --noconfirm aria2
 yay -S --needed --noconfirm aria2-systemd
 
+# File Sync
+# yay -S --needed --noconfirm dropbox
+yay -S --needed --noconfirm nutstore
+# yay -S --needed --noconfirm megasync
+# yay -S --needed --noconfirm baidunetdisk
+
 # Major Applications
-yay -S --needed --noconfirm anki
-yay -S --needed --noconfirm xmind
-yay -S --needed --noconfirm chrome
-yay -S --needed --noconfirm mcomix
-yay -S --needed --noconfirm dingtalk
-yay -S --needed --noconfirm ulauncher
-yay -S --needed --noconfirm celluloid
-yay -S --needed --noconfirm goldendict
-yay -S --needed --noconfirm wps-office
-yay -S --needed --noconfirm teamviewer
-yay -S --needed --noconfirm font-manager
-yay -S --needed --noconfirm deepin-wine-wechat
-
-# Show off
-yay -S --needed --noconfirm gtop
-yay -S --needed --noconfirm lolcat
-yay -S --needed --noconfirm figlet
-yay -S --needed --noconfirm pfetch
-yay -S --needed --noconfirm neofetch
-yay -S --needed --noconfirm tty-clock
-
-# Programming
-yay -S --needed --noconfirm meld
-yay -S --needed --noconfirm postman
-yay -S --needed --noconfirm nosqlbooster
-yay -S --needed --noconfirm intellij-idea-ultimate-edition
-
-# Tools
-yay -S --needed --noconfirm imagewriter
-yay -S --needed --noconfirm deepin-picker
-yay -S --needed --noconfirm deepin-screenshot
-yay -S --needed --noconfirm deepin-system-monitor
-
+# yay -S --needed --noconfirm anki
+# yay -S --needed --noconfirm xmind
+# yay -S --needed --noconfirm mcomix
+# yay -S --needed --noconfirm dingtalk
+# yay -S --needed --noconfirm celluloid
+# yay -S --needed --noconfirm goldendict
+# yay -S --needed --noconfirm wps-office
+# yay -S --needed --noconfirm teamviewer
+# yay -S --needed --noconfirm font-manager
+# yay -S --needed --noconfirm deepin-wine-wechat
+# 
+# # Show off
+# yay -S --needed --noconfirm gtop
+# yay -S --needed --noconfirm lolcat
+# yay -S --needed --noconfirm figlet
+# yay -S --needed --noconfirm pfetch
+# yay -S --needed --noconfirm neofetch
+# yay -S --needed --noconfirm tty-clock
+# 
+# # Programming
+# yay -S --needed --noconfirm meld
+# yay -S --needed --noconfirm postman
+# yay -S --needed --noconfirm nosqlbooster
+# yay -S --needed --noconfirm intellij-idea-ultimate-edition
+# 
+# # Tools
+# yay -S --needed --noconfirm imagewriter
+# yay -S --needed --noconfirm deepin-picker
+# yay -S --needed --noconfirm deepin-screenshot
+# yay -S --needed --noconfirm deepin-system-monitor
+# 
--- starters/theming.sh
+++ starters/theming.sh
@@ -8,11 +8,16 @@ xfconf-query -c xsettings -p /Net/IconThemeName -s "deepin"
 # yay -S --needed --noconfirm tela-icon-theme-git
 # xfconf-query -c xsettings -p /Net/IconThemeName -s "Tela-red-dark"
 
-# Font Setting
-yay -S --needed --noconfirm otf-san-francisco
-yay -S --needed --noconfirm ttf-meslo
-yay -S --needed --noconfirm ttf-monaco
+# Basic Font
+yay -S --needed --noconfirm ttf-dejavu
 yay -S --needed --noconfirm wqy-microhei
 yay -S --needed --noconfirm ttf-nerd-fonts-symbols
+yay -S --needed --noconfirm unicode-emoji
+
+# AUR Font
+yay -S --needed --noconfirm otf-san-francisco
+yay -S --needed --noconfirm ttf-monaco
+yay -S --needed --noconfirm ttf-meslo
+
 # xfconf-query -c xsettings -p /Gtk/FontName -s "SF Pro Text 11"
 # xfconf-query -c xsettings -p /Gtk/MonospaceFontName -s "Monaco 10"
