a7246d9474f9102191f3058fe5e9a5529567c2fa
Erystasius <Felix.E.Huang@gmail.com>
2021-04-02 08:41:42 +0800

    No-message commit [Generated by ac command]

--- bin/install/aur.sh
+++ bin/install/aur.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Enhancement
+# Dev tools
 yay -S --needed --noconfirm base-devel
+
+# Aur packages
 yay -S --needed --noconfirm devour
 yay -S --needed --noconfirm google-chrome
 yay -S --needed --noconfirm oh-my-zsh-git
+yay -S --needed --noconfirm wudao-dict-git
