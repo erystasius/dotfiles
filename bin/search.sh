11185e5727c4232b34c6fe5a738f2332a1e26792
Erystasius <Felix.E.Huang@gmail.com>
2021-04-08 12:05:29 +0800

    No-message commit [Generated by ac command]

--- bin/search.sh
+++ bin/search.sh
@@ -2,27 +2,29 @@
 set -euo pipefail
 
 declare -A options=(
-    [google]='chromium https://google.com/search?q=$word'
-    [duckduckgo]='chromium https://duckduckgo.com?q=$word'
-    [baidu]='chromium https://baidu.com/s?wd=$word'
-    [zhihu]='chromium https://zhihu.com/search?q=$word'
-    [jd]='chromium https://search.jd.com/Search?keyword=$word'
-    [taobao]='chromium https://s.taobao.com/search?q=$word'
-    [github]='chromium https://github.com/search?q=$word'
-    [zdic]='chromium https://zdic.net/hans/$word'
-    [bilibili]='chromium https://search.bilibili.com/all?keyword=$word'
-    [youtube]='chromium https://youtube.com/results?search_query=$word'
     [wudao]='alacritty --hold -e wd $word'
-    [smzdm]='chromium https://search.smzdm.com?s=$word'
-    [douban]='chromium https://douban.com/search?q=$word'
-    [docker]='chromium https://hub.docker.com/search?type=image\&q=$word'
-    [maven]='chromium https://mvnrepository.com/search?q=$word'
-    [forvo]='chromium https://zh.forvo.com/search/$word/'
-    [wikipedia]='chromium https://en.wikipedia.org/wiki/Special:Search/$word'
-    [wikipedia_zh]='chromium https://zh.wikipedia.org/wiki/Special:Search/$word'
-    [ruanyifeng]='chromium https://duckduckgo.com/?q=site%3Aruanyifeng.com+$word'
-    [amap]='chromium https://amap.com/search?query=$word'
+    [google]='$browser https://google.com/search?q=$word'
+    [duckduckgo]='$browser https://duckduckgo.com?q=$word'
+    [baidu]='$browser https://baidu.com/s?wd=$word'
+    [zhihu]='$browser https://zhihu.com/search?q=$word'
+    [jd]='$browser https://search.jd.com/Search?keyword=$word'
+    [taobao]='$browser https://s.taobao.com/search?q=$word'
+    [github]='$browser https://github.com/search?q=$word'
+    [zdic]='$browser https://zdic.net/hans/$word'
+    [bilibili]='$browser https://search.bilibili.com/all?keyword=$word'
+    [youtube]='$browser https://youtube.com/results?search_query=$word'
+    [smzdm]='$browser https://search.smzdm.com?s=$word'
+    [douban]='$browser https://douban.com/search?q=$word'
+    [docker]='$browser https://hub.docker.com/search?type=image\&q=$word'
+    [maven]='$browser https://mvnrepository.com/search?q=$word'
+    [forvo]='$browser https://zh.forvo.com/search/$word/'
+    [wikipedia]='$browser https://en.wikipedia.org/wiki/Special:Search/$word'
+    [wikipedia_zh]='$browser https://zh.wikipedia.org/wiki/Special:Search/$word'
+    [ruanyifeng]='$browser https://duckduckgo.com/?q=site%3Aruanyifeng.com+$word'
+    [amap]='$browser https://amap.com/search?query=$word'
 )
+
+browser=google-chrome-stable
 
 word=$(zenity --entry --text "Type the word you want to search:" --width 500)
 
