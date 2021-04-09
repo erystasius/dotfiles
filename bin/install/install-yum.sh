8482f990c470ef7de7472af0bef3c28f1ec39241
Erystasius <Felix.E.Huang@gmail.com>
2021-04-09 15:42:49 +0800

    No-message commit [Generated by ac command]

--- bin/install/install-yum.sh
+++ bin/install/install-yum.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo ">>>>>>> System update."
-yum update -y
-
 echo ">>>>>>> Add repos."
 yum install -y epel-release
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
@@ -32,27 +29,43 @@ echo ">>>>>>> Installing binary packages."
 
 cd /tmp
 
-fd_release=fd-v8.2.1-x86_64-unknown-linux-musl
-wget https://github.com/sharkdp/fd/releases/download/v8.2.1/$fd_release.tar.gz
-tar xzvf $fd_release.tar.gz
-cp $fd_release/fd /usr/local/bin
+if [ $(which fd) = "" ]; then
+    fd_release=fd-v8.2.1-x86_64-unknown-linux-musl
+    wget https://github.com/sharkdp/fd/releases/download/v8.2.1/$fd_release.tar.gz
+    tar xzvf $fd_release.tar.gz
+    cp $fd_release/fd /usr/local/bin
+fi
 
+
+if [ $(which bat) = "" ]; then
 bat_release=bat-v0.18.0-x86_64-unknown-linux-musl
 wget https://github.com/sharkdp/bat/releases/download/v0.18.0/$bat_release.tar.gz
 tar xzvf $bat_release.tar.gz
 cp $bat_release/bat /usr/local/bin
+fi
 
+if [ $(which exa) = "" ]; then
 exa_release=exa-linux-x86_64-musl-v0.10.0
 wget https://github.com/ogham/exa/releases/download/v0.10.0/$exa_release.zip
 unzip -o -d exa $exa_release.zip
 cp exa/bin/exa /usr/local/bin
+fi
 
+if [ $(which fzf) = "" ]; then
 fzf_release=fzf-0.27.0-linux_amd64
 wget https://github.com/junegunn/fzf/releases/download/0.27.0/$fzf_release.tar.gz
 tar xzvf $fzf_release.tar.gz
 cp fzf /usr/local/bin
+fi
 
-# todo
-# sync bash / fish / git / vim config file
-git clone https://github.com/erystasius/dotfiles.git ~/dotfiles
-sh ~/dotfiles/install-cli
+if [ -d ~/dotfiles ]; then
+    git clone https://github.com/erystasius/dotfiles.git ~/dotfiles
+else
+    cd ~/dotfiles
+    git pull
+fi
+
+~/dotfiles/install-cli
+
+
+
