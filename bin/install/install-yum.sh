ce9284d5f73fcaed64434a861c7d17f9984a4e4f
Erystasius <Felix.E.Huang@gmail.com>
2021-04-09 18:07:22 +0800

    No-message commit [Generated by ac command]

--- bin/install/install-yum.sh
+++ bin/install/install-yum.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo ">>>>>>> Add repos."
+echo ">>>>>>> Add EPEL."
 yum install -y epel-release
-yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-yum-config-manager --add-repo=https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
-yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
+
+echo ">>>>>>> Add custom repos."
+cd /etc/yum.repos.d
+
+if [ -e carlwgeorge-ripgrep-epel-7.repo ]; then
+    yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+fi
+
+if [ -e shells:fish:release:3.repo ]; then
+    yum-config-manager --add-repo=https://download.opensuse.org/repositories/shells:fish:release:3/CentOS_7/shells:fish:release:3.repo
+fi
+
+if [ -e konimex-neofetch-epel-7.repo ]; then
+    yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
+fi
+
 
 echo ">>>>>>> Installing yum packages."
 yum install -y \
@@ -29,7 +42,7 @@ echo ">>>>>>> Installing binary packages."
 
 cd /tmp
 
-if [ $(which fd) = "" ]; then
+if command -v fd; then
     fd_release=fd-v8.2.1-x86_64-unknown-linux-musl
     wget https://github.com/sharkdp/fd/releases/download/v8.2.1/$fd_release.tar.gz
     tar xzvf $fd_release.tar.gz
@@ -37,25 +50,25 @@ if [ $(which fd) = "" ]; then
 fi
 
 
-if [ $(which bat) = "" ]; then
-bat_release=bat-v0.18.0-x86_64-unknown-linux-musl
-wget https://github.com/sharkdp/bat/releases/download/v0.18.0/$bat_release.tar.gz
-tar xzvf $bat_release.tar.gz
-cp $bat_release/bat /usr/local/bin
+if command -v bat; then
+    bat_release=bat-v0.18.0-x86_64-unknown-linux-musl
+    wget https://github.com/sharkdp/bat/releases/download/v0.18.0/$bat_release.tar.gz
+    tar xzvf $bat_release.tar.gz
+    cp $bat_release/bat /usr/local/bin
 fi
 
-if [ $(which exa) = "" ]; then
-exa_release=exa-linux-x86_64-musl-v0.10.0
-wget https://github.com/ogham/exa/releases/download/v0.10.0/$exa_release.zip
-unzip -o -d exa $exa_release.zip
-cp exa/bin/exa /usr/local/bin
+if command -v exa; then
+    exa_release=exa-linux-x86_64-musl-v0.10.0
+    wget https://github.com/ogham/exa/releases/download/v0.10.0/$exa_release.zip
+    unzip -o -d exa $exa_release.zip
+    cp exa/bin/exa /usr/local/bin
 fi
 
-if [ $(which fzf) = "" ]; then
-fzf_release=fzf-0.27.0-linux_amd64
-wget https://github.com/junegunn/fzf/releases/download/0.27.0/$fzf_release.tar.gz
-tar xzvf $fzf_release.tar.gz
-cp fzf /usr/local/bin
+if command -v fzf; then
+    fzf_release=fzf-0.27.0-linux_amd64
+    wget https://github.com/junegunn/fzf/releases/download/0.27.0/$fzf_release.tar.gz
+    tar xzvf $fzf_release.tar.gz
+    cp fzf /usr/local/bin
 fi
 
 if [ -d ~/dotfiles ]; then
