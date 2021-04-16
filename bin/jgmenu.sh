d951c0884921deddb16b84ba8ea5d337ef5eedcc
Erystasius <Felix.E.Huang@gmail.com>
2021-04-16 16:32:44 +0800

    No-message commit [Generated by ac command]

--- bin/jgmenu.sh
+++ bin/jgmenu.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
+# Get window class under cursor.
 wm_class=$(xdotool getmouselocation --shell | grep WINDOW | awk -F '=' '{print $2}' | xargs -I {} xprop -id {} WM_CLASS) 
 
-
-if echo $wm_class | grep -q -e root -e bspwm; then
+# Call jgmenu if window class is root or polybar.
+if echo $wm_class | grep -q -e root -e polybar; then
     jgmenu_run
 fi 
