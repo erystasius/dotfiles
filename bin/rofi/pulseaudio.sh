ddc83c6a0e6b0dbe9054dd50eb12485a8bff8b51
Erystasius <Felix.E.Huang@gmail.com>
2021-07-30 19:41:23 +0800

    No-message commit [Generated by ac command]

--- bin/rofi/pulseaudio.sh
+++ bin/rofi/pulseaudio.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-function list_options() {
+function list-options() {
     pactl list short sinks | cut -f 2
 }
 
-function do_action() {
+function do-action() {
     pactl set-default-sink $1
     pactl list short sink-inputs | cut -f 1 | xargs -I {} pactl move-sink-input {} $1
 }
 
 if [ -z "$@" ]; then
-    list_options    
+    list-options    
 else
-    do_action $1
+    do-action $1
 fi
